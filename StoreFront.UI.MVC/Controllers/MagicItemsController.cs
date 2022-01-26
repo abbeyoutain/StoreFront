fa83f3bf5cfccf726cf0abf73c8d811d7fffe965
Abbey Outain <abbeyoutain@outlook.com>
2022-01-26 00:44:50 -0600

    Completed image upload utility

--- StoreFront.UI.MVC/Controllers/MagicItemsController.cs
+++ StoreFront.UI.MVC/Controllers/MagicItemsController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Drawing; //added for Image
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using StoreFront.DATA.EF;
+using StoreFront.UI.MVC.Utilities; //added for access to ImageUtility class
 
 namespace StoreFront.UI.MVC.Controllers
 {
@@ -51,10 +53,41 @@ namespace StoreFront.UI.MVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MagicItemID,MagicItemName,Description,CategoryID,RarityID,Price,StatusID,MakerID,MagicItemImage")] MagicItem magicItem)
+        public ActionResult Create([Bind(Include = "MagicItemID,MagicItemName,Description,CategoryID,RarityID,Price,StatusID,MakerID,MagicItemImage")] MagicItem magicItem, HttpPostedFileBase itemImage)
         {
             if (ModelState.IsValid)
             {
+
+                //Image Upload utility step 6
+                #region File Upload
+                string file = "NoImage.png";
+
+                if (itemImage != null)
+                {
+                    //Process the file that was uploaded by the user
+                    file = itemImage.FileName;
+                    string ext = file.Substring(file.LastIndexOf('.'));
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+                    //This if checks to see if the file uploaded is the right type of file
+                    //i.e. file extension would be included in the goodExts
+                    //We will also require the file uploaded to be 4 MB or less in size
+                    if (goodExts.Contains(ext.ToLower()) && itemImage.ContentLength <= 4194304)
+                    {
+                        //create a new file name using a GUID (Globally Unique Identifier)
+                        file = Guid.NewGuid() + ext;
+
+                        string savePath = Server.MapPath("~/Content/imgstore/books/"); //this is where the images will be saved
+                        Image convertedImage = Image.FromStream(itemImage.InputStream);
+                        int maxImageSize = 500;
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+                    }
+                }
+                //no matter what, update the PhotoUrl with the value of the file variable
+                magicItem.MagicItemImage = file;
+                #endregion
+
                 db.MagicItems.Add(magicItem);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -91,10 +124,44 @@ namespace StoreFront.UI.MVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MagicItemID,MagicItemName,Description,CategoryID,RarityID,Price,StatusID,MakerID,MagicItemImage")] MagicItem magicItem)
+        public ActionResult Edit([Bind(Include = "MagicItemID,MagicItemName,Description,CategoryID,RarityID,Price,StatusID,MakerID,MagicItemImage")] MagicItem magicItem, HttpPostedFileBase itemImage)
         {
             if (ModelState.IsValid)
             {
+                //Image Upload utility step 10
+                #region File Upload
+                if (itemImage != null)
+                {
+                    //get file name
+                    string file = itemImage.FileName;
+
+                    //get the file extension
+                    string ext = file.Substring(file.LastIndexOf('.'));
+
+                    //create a list of good extensions
+                    string[] goodExts = { ".jpeg", ".jpg", ".png", ".gif" };
+
+                    if (goodExts.Contains(ext.ToLower()) && itemImage.ContentLength <= 4194304)
+                    {
+                        file = Guid.NewGuid() + ext;
+                        string savePath = Server.MapPath("~/Content/images/");
+                        Image convertedImage = Image.FromStream(itemImage.InputStream);
+                        int maxImageSize = 500;
+                        int maxThumbSize = 100;
+
+                        ImageUtility.ResizeImage(savePath, file, convertedImage, maxImageSize, maxThumbSize);
+
+                        if (magicItem.MagicItemImage != null && magicItem.MagicItemImage != "NoImage.png")
+                        {
+                            string path = Server.MapPath("~/Content/images/");
+                            ImageUtility.Delete(path, magicItem.MagicItemImage);
+                        }
+
+                        magicItem.MagicItemImage = file; //this updates the book object before it is saved to the DB with the latest file name
+                    }
+                }
+                #endregion
+
                 db.Entry(magicItem).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
