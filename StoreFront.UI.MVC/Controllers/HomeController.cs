0efee13519845164d88c9a145ed1bc3ab1c61cd5
Abbey Outain <abbeyoutain@outlook.com>
2022-01-27 01:17:19 -0600

    Added Contact Form functionality

--- StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using StoreFront.UI.MVC.Models;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace StoreFront.UI.MVC.Controllers
 {
@@ -13,9 +18,56 @@ namespace StoreFront.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"{cvm.Name} has sent you the following message from {cvm.Email}:<br/>Subject: {cvm.Subject}<br/>Message: {cvm.Message}.<br/>(This was sent from your Storefront Application.)";
+
+            //MailMessage
+            MailMessage mm = new MailMessage(
+                ConfigurationManager.AppSettings["EmailUser"].ToString(),
+                ConfigurationManager.AppSettings["EmailTo"].ToString(),
+                cvm.Subject,
+                message
+                );
+
+            //MailMessage properties
+            //Allow HTML in the email
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            //Respond to the sender, not our website
+            mm.ReplyToList.Add(cvm.Email);
+
+            //SmtpClient - Info from the host that allows this to be sent
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings["EmailClient"].ToString());
+            //client.Port = 25; //alternative port number 8889
+            //client.EnableSsl = false;
+            //^add if errors occur
+
+            //Client Credentials
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["EmailUser"].ToString(), ConfigurationManager.AppSettings["EmailPass"].ToString());
+
+            //Try to send the email
+            try
+            {
+                //attempt to send
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message:<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
